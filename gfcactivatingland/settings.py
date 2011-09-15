9f3d985c2d9c09d91cffd72961a69cd0df72a562
Douglas Meehan <dmeehan@gmail.com>
2011-09-15 13:10:39 -0400

    edited staticfiles settings

--- gfcactivatingland/settings.py
+++ gfcactivatingland/settings.py
@@ -46,9 +46,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = (
-   'staticfiles.storage.StaticFileStorage',
-)
+STATICFILES_STORAGE = 'staticfiles.storage.StaticFileStorage'
 
 STATICFILES_FINDERS = (
    'django.contrib.staticfiles.finders.FileSystemFinder',
